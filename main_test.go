e0d8257b13237448b4fde4eff867b3c2d0d0f3d0
BT <BT@mail.ru>
2020-01-09 22:15:44 +0500

    Fixed #5

--- main_test.go
+++ main_test.go
@@ -10,6 +10,8 @@ func Test_fuelConsumption(t *testing.T) {
 		want        int
 	}{
 		{name: "How much fuel is enough for kilometers", fuel: 100, consumption: 24, want: 395},
+		{name: "When consumption equals existing amount", fuel: 24, consumption: 24, want: 95},
+		{name: "When consumption greater than existing amount", fuel: 10, consumption: 24, want: 39},
 	}
 	for _, tests := range tests {
 		got := fuelConsumption(tests.fuel, tests.consumption)
