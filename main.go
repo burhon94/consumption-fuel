3f1781f560280cc80a88953f4591c063efe3434b
BT <BT@mail.ru>
2020-01-09 18:41:51 +0500

    alignment applied

--- main.go
+++ main.go
@@ -8,6 +8,6 @@ func fuelConsumption(litr, consumption int) int {
 	const per_km = 100
 	distances := 0
 	const safedistance = 5
-	distances = (per_km - safedistance) * litr  / consumption
+	distances = (per_km - safedistance) * litr / consumption
 	return distances
 }
