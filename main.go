1f4e715c775d8f3c14302cf9f55c4ca960b49e81
BT <BT@mail.ru>
2020-01-09 21:49:02 +0500

    Fixed #1

--- main.go
+++ main.go
@@ -4,10 +4,10 @@ func main() {
 
 }
 
-func fuelConsumption(litr, consumption int) int {
+func fuelConsumption(liter, consumption int) int {
 	const per_km = 100
 	distances := 0
 	const safedistance = 5
-	distances = (per_km - safedistance) * litr / consumption
+	distances = (per_km - safedistance) * liter / consumption
 	return distances
 }
